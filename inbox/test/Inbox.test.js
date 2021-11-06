f2a9bdacc521acd5c4a5e2e0f58414cc863e1b75
Allan Azevedo <70598205+Bushidonj@users.noreply.github.com>
2021-11-06 11:55:47 -0300

    step 3

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -7,15 +7,16 @@ const { interface, bytecode } = require("../compile");
 let accounts;
 let inbox;
 
+
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ["PAO DE QUEIJO"],
+      arguments: ['PAO DE QUEIJO'],
     })
-    .send({ from: accounts[0], gas: "1000000" });
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe("Inbox", () => {
@@ -24,7 +25,7 @@ describe("Inbox", () => {
   });
   it("has a default message", async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, "Hi there!");
+    assert.equal(message, 'PAO DE QUEIJO');
   });
   it("can change the message", async () => {
     await inbox.methods.setMessage("bye").send({ from: accounts[0] });
